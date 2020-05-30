cf6ceb78f7f5eb3b42c8f7575674153d65ae8a11
Devon Rathie-Wright <devon.w@gmail.com>
2020-05-30 10:12:23 -0600

    back in business (#4)

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Element Nova</title>
+  </head>
+  <body>
+  <h1>Element Nova</h1>
+  <p>Coming Soon. &gt;<span style="font-size:20px">❋</span>&lt;</p>
+  </body>
+</html>
